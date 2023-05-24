aaddd48c5cb0812215239419ecc6abb0e6d12bf0
Lakshmy <Lakshmy25rahul@gmail.com>
2023-05-24 14:00:27 +0530

    main footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { CategoryComponent } from './category/category.component';
 import { SpecialofferComponent } from './specialoffer/specialoffer.component';
 import { SearchproductComponent } from './searchproduct/searchproduct.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SearchproductComponent } from './searchproduct/searchproduct.component'
     NavbarComponent,
     CategoryComponent,
     SpecialofferComponent,
-    SearchproductComponent
+    SearchproductComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
